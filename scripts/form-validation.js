093a0b5e4faa3da4b37658a09f8061576a39099c
F488-DESKTOP <87024293+brunettisergio@users.noreply.github.com>
2024-07-17 14:42:11 -0400

    Added form-validation.js file

---
+++ scripts/form-validation.js
@@ -0,0 +1,5 @@
+const nameWidget = document.querySelector("#name"),
+    phoneNumberWidget = document.querySelector("#phoneNumber"),
+    emailWidget = document.querySelector("#email"),
+    petNameWidget = document.querySelector("#petName"),
+    petAgeWidget = document.querySelector("#petAge");
